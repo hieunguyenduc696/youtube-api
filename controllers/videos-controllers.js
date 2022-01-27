a683c48f9709c572de200570eeeab917187f54b9
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-27 17:30:43 +0700

    adding controllers

---
+++ controllers/videos-controllers.js
@@ -0,0 +1,44 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_VIDEOS = [
+  {
+    id: "v1",
+    image:
+      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
+    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
+    description:
+      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
+    author: "u1",
+    views: 4.7,
+    createdAt: "8 months ago",
+    likes: 626,
+  },
+];
+
+const getVideoById = (req, res, next) => {
+  const videoId = req.params.vid;
+
+  const video = DUMMY_VIDEOS.find((v) => v.id === videoId);
+
+  if (!video) {
+    throw new HttpError("Could not find a video for the provided id.", 404);
+  }
+
+  res.json({ video });
+};
+
+const getVideosByUserId = (req, res, next) => {
+  const userId = req.params.uid;
+  const videos = DUMMY_VIDEOS.filter((v) => v.author === userId);
+
+  if (!videos || videos.length === 0) {
+    return next(
+      new HttpError("Could not find videos for the provided user id.", 404)
+    );
+  }
+
+  res.json({ videos });
+};
+
+exports.getVideoById = getVideoById;
+exports.getVideosByUserId = getVideosByUserId;
--- routes/videos-routes.js
+++ routes/videos-routes.js
@@ -1,47 +1,11 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
+const videosControllers = require('../controllers/videos-controllers')
 
 const router = express.Router();
 
-const DUMMY_VIDEOS = [
-  {
-    id: "v1",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "u1",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-];
+router.get("/:vid", videosControllers.getVideoById);
 
-router.get("/:vid", (req, res, next) => {
-  const videoId = req.params.vid;
-
-  const video = DUMMY_VIDEOS.find((v) => v.id === videoId);
-
-  if (!video) {
-    throw new HttpError("Could not find a video for the provided id.", 404);
-  }
-
-  res.json({ video });
-});
-
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const videos = DUMMY_VIDEOS.filter((v) => v.author === userId);
-
-  if (!videos || videos.length === 0) {
-    return next(
-      new HttpError("Could not find videos for the provided user id.", 404)
-    );
-  }
-
-  res.json({ videos });
-});
+router.get("/user/:uid", videosControllers.getVideosByUserId);
 
 module.exports = router;
