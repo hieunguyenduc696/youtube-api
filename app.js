aa50f7e052ae84a3003741b89d3141e706c3ceba
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-27 17:11:01 +0700

    adding video-specific Routes

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const videosRoutes = require("./routes/videos-routes");
 
 const app = express();
 
-app.use(videosRoutes);
+app.use('/api/videos', videosRoutes);
 
 app.listen(5000);
--- routes/videos-routes.js
+++ routes/videos-routes.js
@@ -2,9 +2,26 @@ const express = require("express");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET request in Videos");
-  res.json({ message: "It works!" });
+const DUMMY_VIDEOS = [
+  {
+    id: "v1",
+    image:
+      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
+    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
+    description:
+      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
+    author: "u1",
+    views: 4.7,
+    createdAt: "8 months ago",
+    likes: 626,
+  },
+];
+
+router.get("/:vid", (req, res, next) => {
+  const videoId = req.params.vid;
+  
+  const video = DUMMY_VIDEOS.find(v => v.id === videoId)
+  res.json({ video });
 });
 
-module.exports = router
+module.exports = router;
