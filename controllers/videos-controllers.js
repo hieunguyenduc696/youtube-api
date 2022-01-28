e5d156147b4c388b4fd49beabdc80fa1c20d46f1
Nguyen Duc Hieu <hieunguyenduc696@gmail.com>
2022-01-28 17:11:26 +0700

    finishing mongoose part

--- controllers/videos-controllers.js
+++ controllers/videos-controllers.js
@@ -5,21 +5,6 @@ const HttpError = require("../models/http-error");
 const Video = require("../models/video");
 const User = require("../models/user");
 
-let DUMMY_VIDEOS = [
-  {
-    id: "v1",
-    image:
-      "https://i.ytimg.com/vi/uZfcxvrsL28/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCGh7xxQbMgq8h-39aXVLVjDym0Vw",
-    title: "Neymar Jr & Ronaldinho Most Creative & Smart Plays",
-    description:
-      "Please Subscribe if you Enjoy my videos it gives me motivation to make more videos. Turn notifications on and you will never miss a video again.",
-    author: "u1",
-    views: 4.7,
-    createdAt: "8 months ago",
-    likes: 626,
-  },
-];
-
 const getVideoById = async (req, res, next) => {
   const videoId = req.params.vid;
 
